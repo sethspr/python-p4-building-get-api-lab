2abdeaed23c86868593df4c7eda17bb82bc9e409
seth spring <seth.c.spring@gmail.com>
2024-04-04 17:28:44 -0400

    COMPLETED - fun!

--- server/app.py
+++ server/app.py
@@ -20,19 +20,37 @@ def index():
 
 @app.route('/bakeries')
 def bakeries():
-    return ''
+    bakeries = Bakery.query.all()
+
+    all_bakeries = []
+    for bakes in bakeries:
+        all_bakeries.append(bakes.to_dict())
+    return all_bakeries, 200
 
 @app.route('/bakeries/<int:id>')
 def bakery_by_id(id):
-    return ''
+    bakery = Bakery.query.filter(Bakery.id == id).first()
+
+    if bakery == None:
+        return {'error': 'Bakery does not exist in db'}, 404
+    
+    return bakery.to_dict(), 200        
 
 @app.route('/baked_goods/by_price')
 def baked_goods_by_price():
-    return ''
+    pricing = BakedGood.query.order_by(BakedGood.price.desc()).all()
+
+    bakery_prices = []
+    for prices in pricing:
+        bakery_prices.append(prices.to_dict())
+    return bakery_prices, 200
+
 
 @app.route('/baked_goods/most_expensive')
 def most_expensive_baked_good():
-    return ''
+    pricing = BakedGood.query.order_by(BakedGood.price.desc()).limit(1).first()
+
+    return pricing.to_dict(), 200
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
